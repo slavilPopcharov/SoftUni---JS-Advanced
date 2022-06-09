31c6571dbb528e8d5ff4891320f57c6ca0003fdc
Slavil <103033390+slavilPopcharov@users.noreply.github.com>
2022-06-09 11:40:56 +0300

    Add files via upload

---
+++ 03. Objects and composition/Exercise/10. Heroes.js
@@ -0,0 +1,41 @@
+function solve() {
+
+  const canCast = (state) => ({
+      cast: (spell) => {
+          console.log(`${state.name} cast ${spell}`);
+          state.mana--;
+      }
+  });
+
+  const canFight = (state) => ({
+      fight: () => {
+          console.log(`${state.name} slashes at the foe!`);
+          state.stamina--;
+      }
+  })
+
+  const mage = (name) => {
+      let state = {
+          name,
+          health: 100,
+          mana: 100,
+      }
+
+      return Object.assign(state, canCast(state));
+  }
+
+  function fighter(name) {
+      let state = {
+          name,
+          health: 100,
+          stamina: 100,
+      }
+
+      return Object.assign(state, canFight(state));
+  }
+
+  return {
+      mage,
+      fighter
+  }
+}
